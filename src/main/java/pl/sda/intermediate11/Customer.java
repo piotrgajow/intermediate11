329a053ebba1ea65446b7314137cac52870839d6
sdakf <sdakftraining@gmail.com>
2018-08-15 09:36:34 +0200

    customers statistics test

---
+++ src/main/java/pl/sda/intermediate11/Customer.java
@@ -0,0 +1,31 @@
+package pl.sda.intermediate11;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Getter
+@Setter
+public class Customer {
+    private Integer id;
+    private String firstName;
+    private String surname;
+    private Integer age;
+    private BigDecimal salary;
+    private static AtomicInteger idCounter = new AtomicInteger(1);
+
+    public Customer(String firstName, String surname, Integer age, Integer salary) {
+        this.id = idCounter.getAndIncrement();
+        this.firstName = firstName;
+        this.surname = surname;
+        this.age = age;
+        this.salary = BigDecimal.valueOf(salary);
+    }
+
+    public Customer(String firstName, String surname, Integer age, String salary) {
+        this(firstName, surname, age, BigDecimal.valueOf(Double.parseDouble(salary)).setScale(2, RoundingMode.HALF_UP).intValue());
+    }
+}
