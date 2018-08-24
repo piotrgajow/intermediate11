ef60d91cf7ad461f49720e92494e1c0071003a8f
sdakf <sdakftraining@gmail.com>
2018-08-24 20:23:42 +0200

    weather model

---
+++ src/main/java/pl/sda/intermediate11/bookstore/weather/model/Coord.java
@@ -0,0 +1,15 @@
+package pl.sda.intermediate11.bookstore.weather.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Coord {
+
+    @SerializedName("lon")
+    @Expose
+    public Double lon;
+    @SerializedName("lat")
+    @Expose
+    public Double lat;
+
+}
---
+++ src/main/java/pl/sda/intermediate11/bookstore/weather/model/Main.java
@@ -0,0 +1,24 @@
+package pl.sda.intermediate11.bookstore.weather.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Main {
+
+    @SerializedName("temp")
+    @Expose
+    public Double temp;
+    @SerializedName("pressure")
+    @Expose
+    public Integer pressure;
+    @SerializedName("humidity")
+    @Expose
+    public Integer humidity;
+    @SerializedName("temp_min")
+    @Expose
+    public Double tempMin;
+    @SerializedName("temp_max")
+    @Expose
+    public Double tempMax;
+
+}
